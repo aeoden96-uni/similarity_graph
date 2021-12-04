70f5703edf9e0090e1eb193d163180870f474ca8
Mateo <mateo1559@gmail.com>
2021-12-04 18:11:20 +0100

    small fixes

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ import numpy as np
 
 
 def find_word_id(word):
-    with open("examples/e00/index.txt",encoding= "ISO-8859-1") as f:
+    with open("examples/e00/index.txt", encoding="ISO-8859-1") as f:
         lines = f.readlines()
 
-        for i,line in enumerate(lines):
+        for i, line in enumerate(lines):
             if word.rstrip() == line.rstrip():
-                return i+1
+                return i + 1
 
         raise EOFError
 
@@ -26,44 +26,41 @@ def load_matrix_from_file(filename):
 
 
 def load_matrix(dim, pairs):
-
     matrix = np.zeros((dim, dim))
     for i, j in pairs:
-            matrix[i-1][j-1] = 1
+        matrix[i - 1][j - 1] = 1
     return matrix
 
 
-
-def create_set_form_lines(lines, id):
+def create_set_form_lines(lines, word_id):
     uniq = set()
     for line in lines:
         try:
             i, j = line.split()
-        except:
+        except ValueError:
             continue
         i = int(i)
         j = int(j)
 
-        if id == i :
+        if word_id == i:
             uniq.add(j)
-        if  id == j:
+        if word_id == j:
             uniq.add(i)
-
 
     return uniq
 
 
-def create_pairs_from_set(lines, S):
+def create_pairs_from_set(lines, word_set):
     pairs = []
 
     for line in lines:
         try:
             i, j = line.split()
-        except:
+        except ValueError:
             continue
         i = int(i)
         j = int(j)
-        if i in S and j in S:
+        if i in word_set and j in word_set:
             pairs.append((i, j))
     return pairs
 
@@ -76,9 +73,9 @@ def create_new_pairs(mapping, pairs):
     return new_pairs
 
 
-def create_mapping(S):
+def create_mapping(word_set):
     mapping = []
-    for u in S:
+    for u in word_set:
         mapping.append(u)
     return mapping
 
@@ -107,10 +104,10 @@ def write_new_index_file(filename, index):
             w.write(i)
 
 
-def load_for_word(ime_dat, rijec_id):
+def load_for_word(ime_dat, word_id):
     with open(ime_dat) as f:
         lines = f.readlines()
-        s = create_set_form_lines(lines, rijec_id)
+        s = create_set_form_lines(lines, word_id)
 
         pairs = create_pairs_from_set(lines, s)
 
@@ -119,90 +116,101 @@ def load_for_word(ime_dat, rijec_id):
 
     new_index = create_new_index(mapping)
 
-    # write_new_pairs_to_file("novi" + str(rijec_id) + ".txt", new_pairs)
-    # write_new_index_file("novi" + str(rijec_id) + "rijeci.txt", mapping)
+    # write_new_pairs_to_file("new" + str(word_id) + ".txt", new_pairs)
+    # write_new_index_file("new" + str(word_id) + "words.txt", mapping)
 
     return len(s), new_index, new_pairs
 
 
-def inter(limit, A, B):
-    X = np.ones((B.shape[0], A.shape[0]))
-    past = X.copy()
-    if limit < 1: raise ValueError
+def calculate(limit, matrix_a, matrix_b):
+    x = np.ones((matrix_b.shape[0], matrix_a.shape[0]))
+    past = x.copy()
+    if limit < 1:
+        raise ValueError
 
     for i in range(50):
         i += 1
-        left_hand = B.dot(X).dot(A.transpose())
-        right_hand = B.transpose().dot(X).dot(A)
-        X = left_hand + right_hand
-        norm = np.linalg.norm(X, 'fro')
-        X = X / norm
+        left_hand = matrix_b.dot(x).dot(matrix_a.transpose())
+        right_hand = matrix_b.transpose().dot(x).dot(matrix_a)
+        x = left_hand + right_hand
+        norm = np.linalg.norm(x, 'fro')
+        x = x / norm
 
-        # print(i, np.around(X, 4)[0][0])
+        # print(i, np.around(x, 4)[0][0])
         if i % 2 == 0:
-            if np.allclose(past, X, rtol=1e-05):
-                return np.around(X, 4)
-            past = X.copy()
-
+            if np.allclose(past, x, rtol=1e-05):
+                return np.around(x, 4)
+            past = x.copy()
 
 
 def main():
     example = input("Example (etc 'e01'): ")
 
     if example == "e00":
-        id = find_word_id(input("Word: "))
-        #print("ID",id)
+        word_id = find_word_id(input("Word: "))
         print("Word OK")
 
-
-        # id = 87802
-
         dim, index, pairs = load_for_word("examples/" + example + "/dico.txt",
-                                          id)
+                                          word_id)
 
         # print(pairs)
 
-        A = load_matrix(dim, pairs)
-        B = load_matrix_from_file("examples/e00/B.txt")
+        matrix_first = load_matrix(dim, pairs)
+        matrix_second = load_matrix_from_file("examples/e00/B.txt")
 
-        # print("Korijen sume kvadrata elemenata", np.sqrt(np.sum(np.square(p))))
-        p = inter(41, A, B)[1]
+        # print("Root of sum of squares", np.sqrt(np.sum(np.square(p))))
+        p = calculate(41, matrix_first, matrix_second)[1]
 
         results = []
 
-        keywords = ["the" , "or" ,"a" , "in" , "to" , "of" ,"which",
-                    "and","as","with", "for","any","it","by", "is","are","than",
-                    "an", "from","into", "be"]
+        keywords = ["the",
+                    "or",
+                    "a",
+                    "in",
+                    "to",
+                    "of",
+                    "which",
+                    "and",
+                    "as",
+                    "with",
+                    "for",
+                    "any",
+                    "it",
+                    "by",
+                    "is",
+                    "are",
+                    "than",
+                    "an",
+                    "from",
+                    "into",
+                    "be"]
 
-        for i,l in enumerate(p):
+        for i, l in enumerate(p):
             results.append((l, index[i]))
 
         results.sort(key=lambda x: x[0])
 
-        max = 10
-        for i,k in results[::-1]:
+        max_iter = 10
+        for i, k in results[::-1]:
             if k.rstrip() in keywords:
                 continue
-            if max <= 0:
+            if max_iter <= 0:
                 break
-            print(i,k, end="")
-            max -=1
-
+            print(i, k, end="")
+            max_iter -= 1
 
     else:
 
+        matrix_first = load_matrix_from_file(
+            "examples/" + example + "/matrix_first.txt")
+        # print(matrix_first)
+        matrix_second = load_matrix_from_file(
+            "examples/" + example + "/matrix_second.txt")
 
-        A = load_matrix_from_file("examples/" + example + "/A.txt")
-        # print(A)
-        B = load_matrix_from_file("examples/" + example + "/B.txt")
-
-        p = inter(1,A,B)
+        p = calculate(1, matrix_first, matrix_second)
 
         print(p)
-        # print("Korijen sume kvadrata elemenata", np.sqrt(np.sum(np.square(p))))
-
-
-
+        # print("Root of sum of squares", np.sqrt(np.sum(np.square(p))))
 
 
 if __name__ == '__main__':
